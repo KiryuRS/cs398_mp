0f9d6749e88e7c1be388e6d1f7c29c7ec11288d1
Kenneth Toh <ktwgkiryu@outlook.sg>
2018-11-28 17:47:31 +0800

    Updated framework

---
+++ fractalCUDA/Common.h
@@ -0,0 +1,51 @@
+#pragma once
+
+///////////////////////////////////////////////////////////////////////////
+// Common Types, Defines and Includes
+///////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdint>
+#include <fstream>
+#include "bmp.h"
+
+typedef unsigned int				uint;
+typedef unsigned char				uchar;
+typedef unsigned short				ushort;
+typedef unsigned long				ulong;
+typedef long long					llong;
+
+#ifndef MAX
+#define MAX(a,b) \
+((a > b) ? a : b)
+#endif
+
+#ifndef MIN
+#define MIN(a,b) \
+((a < b) ? a : b)
+#endif
+
+#define UINT_BITS					32
+
+///////////////////////////////////////////////////////////////////////////
+// CUDA Includes
+///////////////////////////////////////////////////////////////////////////
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <helper_cuda.h>
+#include <helper_functions.h>
+
+///////////////////////////////////////////////////////////////////////////
+// Helper Function Calls and Misc.
+///////////////////////////////////////////////////////////////////////////
+template <typename InputIter, typename OutputIter>
+OutputIter MyCopy(InputIter begin, InputIter end, OutputIter start)
+{
+	while (begin != end)
+		*start++ = *begin++;
+	return start;
+}
---
+++ fractalCUDA/main.cpp
@@ -0,0 +1,172 @@
+// @TODO: Uncomment the codes below to run the relevant test case
+#define YONGKIAT_VERSION
+//#define ALVIN_VERSION
+//#define CHENGJIANG_VERSION
+//#define KENNETH_VERSION
+
+#define PIXELDIM		512
+static constexpr size_t PIXELDIM3 =	PIXELDIM * PIXELDIM * 3;
+
+#include "Common.h"
+
+void PrintInformation(cudaDeviceProp& deviceProp)
+{
+	int driverVersion = 0, runtimeVersion = 0;
+	cudaDriverGetVersion(&driverVersion);
+	cudaRuntimeGetVersion(&runtimeVersion);
+	printf("  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n",
+		driverVersion / 1000, (driverVersion % 100) / 10,
+		runtimeVersion / 1000, (runtimeVersion % 100) / 10);
+	printf("  CUDA Capability Major/Minor version number:    %d.%d\n",
+		deviceProp.major, deviceProp.minor);
+	printf("  Total amount of global memory:                 %.2f GBytes (%llu "
+		"bytes)\n", (float)deviceProp.totalGlobalMem / pow(1024.0, 3),
+		(unsigned long long)deviceProp.totalGlobalMem);
+	printf("  GPU Clock rate:                                %.0f MHz (%0.2f "
+		"GHz)\n", deviceProp.clockRate * 1e-3f,
+		deviceProp.clockRate * 1e-6f);
+	printf("  Memory Clock rate:                             %.0f Mhz\n",
+		deviceProp.memoryClockRate * 1e-3f);
+	printf("  Memory Bus Width:                              %d-bit\n",
+		deviceProp.memoryBusWidth);
+
+	if (deviceProp.l2CacheSize)
+	{
+		printf("  L2 Cache Size:                                 %d bytes\n",
+			deviceProp.l2CacheSize);
+	}
+
+	printf("  Max Texture Dimension Size (x,y,z)             1D=(%d), "
+		"2D=(%d,%d), 3D=(%d,%d,%d)\n", deviceProp.maxTexture1D,
+		deviceProp.maxTexture2D[0], deviceProp.maxTexture2D[1],
+		deviceProp.maxTexture3D[0], deviceProp.maxTexture3D[1],
+		deviceProp.maxTexture3D[2]);
+	printf("  Max Layered Texture Size (dim) x layers        1D=(%d) x %d, "
+		"2D=(%d,%d) x %d\n", deviceProp.maxTexture1DLayered[0],
+		deviceProp.maxTexture1DLayered[1], deviceProp.maxTexture2DLayered[0],
+		deviceProp.maxTexture2DLayered[1],
+		deviceProp.maxTexture2DLayered[2]);
+	printf("  Total amount of constant memory:               %lu bytes\n",
+		deviceProp.totalConstMem);
+	printf("  Total amount of shared memory per block:       %lu bytes\n",
+		deviceProp.sharedMemPerBlock);
+	printf("  Total number of registers available per block: %d\n",
+		deviceProp.regsPerBlock);
+	printf("  Warp size:                                     %d\n",
+		deviceProp.warpSize);
+	printf("  Maximum number of threads per multiprocessor:  %d\n",
+		deviceProp.maxThreadsPerMultiProcessor);
+	printf("  Maximum number of threads per block:           %d\n",
+		deviceProp.maxThreadsPerBlock);
+	printf("  Maximum sizes of each dimension of a block:    %d x %d x %d\n",
+		deviceProp.maxThreadsDim[0],
+		deviceProp.maxThreadsDim[1],
+		deviceProp.maxThreadsDim[2]);
+	printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d\n",
+		deviceProp.maxGridSize[0],
+		deviceProp.maxGridSize[1],
+		deviceProp.maxGridSize[2]);
+	printf("  Maximum memory pitch:                          %lu bytes\n",
+		deviceProp.memPitch);
+}
+
+int main(int argc, char **argv)
+{
+	// Declaring all the variables
+	StopWatchInterface	*hTimer = nullptr;
+	cudaDeviceProp		deviceProp;
+	bmp_header			header;
+	uchar				cpuOutput[PIXELDIM3]{ };
+	uchar				*gpuOutput = nullptr;
+
+	// Printing the information
+	deviceProp.major = 0;
+	deviceProp.minor = 0;
+	int dev = findCudaDevice(argc, (const char**)argv);
+	checkCudaErrors(cudaGetDeviceProperties(&deviceProp, dev));
+	printf("CUDA device [%s] has %d Multi-Processors, Compute %d.%d\n",
+		   deviceProp.name, deviceProp.multiProcessorCount, deviceProp.major, deviceProp.minor);
+	PrintInformation(deviceProp);
+
+	sdkCreateTimer(&hTimer);
+	sdkDeleteTimer(&hTimer);
+
+	// Some variable naming here
+#ifdef YONGKIAT_VERSION
+	const std::string fileOut{ "_YONGKIAT" };
+#elif defined ALVIN_VERSION
+	const std::string fileOut{ "_ALVIN" };
+#elif defined CHENGJIANG_VERSION
+	const std::string fileOut{ "_CHENGJIANG" };
+#elif defined KENNETH_VERSION
+	const std::string fileOut{ "_KENNETH" };
+#endif
+
+	// CPU CODE HERE
+	sdkResetTimer(&hTimer);
+	sdkStartTimer(&hTimer);
+	// We will run the relevant code based on whose technique it is
+	// SIGNATURE FOR FUNCTION CALL
+	// void FuncName(cpuOutput, gpuOutput)
+#ifdef YONGKIAT_VERSION
+
+#elif defined ALVIN_VERSION
+
+#elif defined CHENGJIANG_VERSION
+
+#elif defined KENNETH_VERSION
+
+#endif
+	sdkStopTimer(&hTimer);
+	float dAvgSecs = 1.0e-3f * (float)sdkGetTimerValue(&hTimer);
+	printf("\nCPU time (average) : %.5f sec, %.4f MB/sec\n", dAvgSecs, ((float)PIXELDIM3 * 1.0e-6f) / dAvgSecs);
+	printf("CPU Fractal, Throughput = %.4f MB/s, Time = %.5f s, Size = %zu Bytes, NumDevsUsed = %u\n",
+		(1.0e-6f * (float)PIXELDIM3 / dAvgSecs), dAvgSecs, PIXELDIM3, 1u);
+
+	// GPU  CODE HERE
+	sdkResetTimer(&hTimer);
+	sdkStartTimer(&hTimer);
+	// We will run the relevant code based on whose technique it is
+	// SIGNATURE FOR FUNCTION CALL
+	// void FuncName(cpuOutput, gpuOutput)
+#ifdef YONGKIAT_VERSION
+
+#elif defined ALVIN_VERSION
+
+#elif defined CHENGJIANG_VERSION
+
+#elif defined KENNETH_VERSION
+
+#endif
+	sdkStopTimer(&hTimer);
+	dAvgSecs = 1.0e-3f * (float)sdkGetTimerValue(&hTimer);
+	printf("\nGPU time (average) : %.5f sec, %.4f MB/sec\n", dAvgSecs, ((float)PIXELDIM3 * 1.0e-6f) / dAvgSecs);
+	printf("GPU Fractal, Throughput = %.4f MB/s, Time = %.5f s, Size = %zu Bytes, NumDevsUsed = %u\n",
+		(1.0e-6f * (float)PIXELDIM3 / dAvgSecs), dAvgSecs, PIXELDIM3, 1u);
+
+#pragma region COPYOUT
+	// Copying the contents over to our output file
+	std::string cpuOutputFile{ "cpuOutput" };
+	cpuOutputFile += fileOut + ".bmp";
+	char* out = new char[PIXELDIM3]{};
+	MyCopy(cpuOutputFile.begin(), cpuOutputFile.end(), out);
+	bmp_write(out, &header, cpuOutput);
+
+	std::string gpuOutputFile{ "gpuOutput" };
+	gpuOutputFile += fileOut + ".bmp";
+	if (!gpuOutput)
+	{
+		std::cerr << "..Please allocate memory for gpuOutput!\nUnable to output file into a bmp file!" << std::endl;
+		return -1;
+	}
+	MyCopy(gpuOutputFile.begin(), gpuOutputFile.end(), out);
+	bmp_write(out, &header, gpuOutput);
+	delete[] out;
+#pragma endregion
+
+	std::string command;
+	command = "start WinMerge " + gpuOutputFile + " " + cpuOutputFile;
+	system(command.c_str());
+
+	return 0;
+}
